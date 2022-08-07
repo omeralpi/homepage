d732fb83963937ef473f88f0cb57b4f1fcb2b933
devalpi <admin@alpi.dev>
2022-08-07 17:45:07 +0300

    chore: remove unused packages

--- next.config.js
+++ next.config.js
@@ -1,17 +1,17 @@
-const { withContentlayer } = require("next-contentlayer");
-
-/**
- * @type {import("next").NextConfig}
- */
-
-module.exports = withContentlayer({
-  swcMinify: true,
-  reactStrictMode: true,
-  images: {
-    domains: ["images.unsplash.com", "dl.airtable.com"],
-  },
-  experimental: {
-    legacyBrowsers: false,
-    browsersListForSwc: true,
-  },
-});
+module.exports = (phase, { defaultConfig }) => {
+  /**
+   * @type {import('next').NextConfig}
+   */
+  const nextConfig = {
+    swcMinify: true,
+    reactStrictMode: true,
+    images: {
+      domains: ["dl.airtable.com"],
+    },
+    experimental: {
+      legacyBrowsers: false,
+      browsersListForSwc: true,
+    },
+  };
+  return nextConfig;
+};
